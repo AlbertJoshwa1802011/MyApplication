8b0135abdde1233c48088a088e68a5e149168548
Albert joshwa <albertjoshrock101@gmail.com>
2023-02-08 00:45:50 +0530

    Completed UI changes on sending flow

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button move;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        move = findViewById(R.id.Move);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,chatBotAi.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void handleSelection(View view) {
